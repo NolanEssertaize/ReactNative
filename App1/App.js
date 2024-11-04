ef464522dda1aa6bf34edfe41aebc2da51a7be88
Nolan Essertaize <n.essertaize@xefi.fr>
2024-11-04 14:38:51 +0100

    Added few button, first modal view and first algo of view

--- App1/App.js
+++ App1/App.js
@@ -1,20 +1,134 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SectionList, Button, Alert ,Modal, Pressable} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+var DATA = [
+  {
+    title: "Exemple",
+    data: ["Sport"]
+  },
+]
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.container} edges={['top']}>
+        <Text style={styles.Text}>Goal</Text>
+          <SectionList
+            sections={DATA}
+            keyExtractor={(item, index) => item + index}
+            renderItem={({item}) => (
+              <View style={styles.item}>
+                <Text style={styles.title}>{item}</Text>
+              </View>
+            )}
+            renderSectionHeader={({section: {title}}) => (
+              <Text style={styles.header}>{title}</Text>
+            )}
+          />
+        </SafeAreaView>
+      </SafeAreaProvider>
+      <View style={styles.bottom_nav_bar}>
+        <Modal
+          title="create"
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Hello World!</Text>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}>
+                <Text style={styles.textStyle}>Hide Modal</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+        <Pressable
+          style={[styles.button, styles.buttonOpen]}
+          onPress={() => setModalVisible(true)}>
+          <Text style={styles.button}>Edit Goal</Text>
+        </Pressable>
+        <Pressable
+          style={[styles.button, styles.buttonOpen]}
+          onPress={() => setModalVisible(true)}>
+          <Text style={styles.button}>Create Goal</Text>
+        </Pressable>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 150,
+    paddingBottom: 100,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 10,
   },
+  item: {
+    backgroundColor: '#696969',
+    padding: 20,
+    marginVertical: 8,
+    borderRadius: 5,
+  },
+  header: {
+    fontSize: 32,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    color: "white",
+  },
+  Text: {
+    fontSize: 40,
+    margin: 10,
+    textAlign: "center",
+    textDecorationLine: "underline"
+  },
+  bottom_nav_bar: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    margin: 30,
+    justifyContent:"space-between"
+    
+  },
+  button: {
+    fontSize: 20,
+    color:"white",
+    padding: 5,
+    borderRadius: 5,
+    textDecorationLine: "underline",
+    backgroundColor: '#B0B0B0'
+  }
+ 
 });
